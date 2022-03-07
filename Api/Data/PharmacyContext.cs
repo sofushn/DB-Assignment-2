4fb7129e279bf3b6e147bd405ef53650f5f3830c
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2022-03-07 19:45:15 +0100

    added audit logger

--- Api/Data/PharmacyContext.cs
+++ Api/Data/PharmacyContext.cs
@@ -8,6 +8,14 @@ public class PharmacyContext: DbContext
     public DbSet<Pharmacy> Pharmacies { get; set; }
     public DbSet<Doctor> Doctors { get; set; }
     public DbSet<Prescription> Prescriptions { get; set; }
-
+    public DbSet<AuditLog> AuditLogs { get; set; }
     public PharmacyContext(DbContextOptions<PharmacyContext> options): base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<AuditLog>()
+            .Property(al => al.Action)
+            .HasConversion<string>();
+    }
 }
